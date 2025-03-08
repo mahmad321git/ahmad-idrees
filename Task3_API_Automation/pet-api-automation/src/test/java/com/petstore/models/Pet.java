9663b83d2ce9e7860847c6949b994dbe78462038
Ahmad Idrees <35655048+mahmad321git@users.noreply.github.com>
2025-03-09 04:25:00 +0500

    data-model

--- Task3_API_Automation/pet-api-automation/src/test/java/com/petstore/models/Pet.java
+++ Task3_API_Automation/pet-api-automation/src/test/java/com/petstore/models/Pet.java
@@ -1,19 +1,24 @@
 package com.petstore.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class Pet {
     private Long id;
     private String name;
     private String status;
-    
-    @JsonProperty("category")
     private Category category;
+    private List<Tag> tags;
     
     @Data
     public static class Category {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    public static class Tag {
         private Long id;
         private String name;
     }
