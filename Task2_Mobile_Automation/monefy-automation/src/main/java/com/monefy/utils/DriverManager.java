88bc186b4bac7dff68b60193866b1c8c16abc523
Ahmad Idrees <35655048+mahmad321git@users.noreply.github.com>
2025-03-09 19:24:32 +0500

    driver-manager

---
+++ Task2_Mobile_Automation/monefy-automation/src/main/java/com/monefy/utils/DriverManager.java
@@ -0,0 +1,21 @@
+// DriverManager.java
+public class DriverManager {
+    private static final ThreadLocal<AppiumDriver> driver = new ThreadLocal<>();
+    
+    public static synchronized AppiumDriver getDriver() {
+        if (driver.get() == null) {
+            initializeDriver();
+        }
+        return driver.get();
+    }
+
+    private static void initializeDriver() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appium:automationName", "UiAutomator2");
+        caps.setCapability("appium:app", Config.APP_PATH);
+        // Add other capabilities
+        
+        driver.set(new AndroidDriver(Config.APPIUM_SERVER_URL, caps));
+    }
+}
